ee7485b1be1ae08eb21c427053ed667d76b711bb
Elliott deLaunay <edelauna@gmail.com>
2024-12-11 03:19:05 +0000

    test(view): basic build test

--- view/src/App.test.tsx
+++ view/src/App.test.tsx
@@ -2,8 +2,13 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+// Mock the Chart component with a named export
+jest.mock('./components/chart.tsx', () => ({
+  Chart: () => <div>Mock Chart</div>
+}));
+
+test('renders learn react link', async () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Check out the project on /i);
   expect(linkElement).toBeInTheDocument();
 });
