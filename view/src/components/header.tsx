43e778e498ee2abbc7aedf4477091bb71365e86d
Elliott de Launay <edelauna@gmail.com>
2025-01-13 23:36:50 -0500

    fix(view/nav-bar): fixing to top of page

--- view/src/components/header.tsx
+++ view/src/components/header.tsx
@@ -17,7 +17,7 @@ export const Header = () => {
       <div className="mx-auto max-w-7xl px-4 py-4 sm:px-6 lg:px-8 flex justify-between items-center">
         <h1 className="text-3xl font-bold tracking-tight text-gray-900 dark:text-white">{ctaCB()}</h1>
         <div className="md:ml-auto md:flex-1 md:max-w-md">
-          <ComboBox /> {/* Assuming ComboBox is another React component */}
+          <ComboBox />
         </div>
       </div>
     </header>
